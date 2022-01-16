e6ecf8f8440f70ba1346652d71006ee540ea66c7
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2022-01-16 11:42:35 +0100

    RestController

--- src/main/java/tek/test/test01/controllers/IndexController.java
+++ src/main/java/tek/test/test01/controllers/IndexController.java
@@ -2,13 +2,14 @@ package tek.test.test01.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
     @GetMapping("/")
     public String home(){
-        return "index.html";
+        return "This is Test01";
     }
 
     @GetMapping("/new")
