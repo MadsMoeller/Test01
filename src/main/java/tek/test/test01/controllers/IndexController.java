8de5a207a00298f5e5444366d6088734d0b5803f
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2022-01-16 11:45:49 +0100

    Normal Controller

--- src/main/java/tek/test/test01/controllers/IndexController.java
+++ src/main/java/tek/test/test01/controllers/IndexController.java
@@ -4,16 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/")
     public String home(){
-        return "This is Test01";
+        return "index.html";
     }
 
+    /*
     @GetMapping("/new")
     public String newIndex(){
         return "Diggy diggy";
     }
+     */
 }
